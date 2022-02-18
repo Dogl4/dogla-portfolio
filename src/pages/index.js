ac9293f90a1aaccf6ecf51f9847fefa9df52a48e
Dogl4 <doougllas@hotmail.com.br>
2022-02-18 17:42:37 -0300

    save point for start about

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,34 +9,34 @@ import Footer from '../components/Footer';
 const Home = () => {
 
   return (
-      <div className="flex justify-center min-h-screen overscroll-auto">
-        <div className="grid grid-cols-12 grid-rows-6 md:h-screen bg-sky-900">
+    <div className="flex justify-center min-h-screen overscroll-auto">
+      <div className="grid grid-cols-12 grid-rows-6 md:h-screen bg-zinc-800">
 
-          <Nav />
+        <Nav />
 
-          <div className="md:row-start-2 row-start-2 row-span-2 col-start-2 col-span-10 rounded flex justify-center">
-            <div className="grid grid-cols-2 gap-2">
-              <div className={`text-white p-2 md:col-span-1 col-span-2 rounded flex justify-center`}>
-                <div className={styles.card}>
-                  <h2 className={styles.title}>Olá,</h2>
-                  <h2>Eu sou Pedro Barreto!</h2>
-                  <p>Desenvolvedor Junior FullStack</p>
-                  <Link href="/about" className={styles.link}>Conheça me</Link>
-                </div>
+        <div className="md:row-start-2 row-start-2 row-span-2 col-start-2 col-span-10 rounded flex justify-center">
+          <div className="grid grid-cols-2 gap-2">
+            <div className={`text-white p-2 md:col-span-1 col-span-2 rounded flex justify-center`}>
+              <div className={styles.card}>
+                <h2 className={styles.title}>Olá,</h2>
+                <h2>Eu sou Pedro Barreto!</h2>
+                <p>Desenvolvedor Junior FullStack</p>
+                <Link href="/about" className={styles.link}>Conheça me</Link>
               </div>
-              <div className="md:col-span-1 hidden rounded md:flex justify-center">
-                <div>
+            </div>
+            <div className="md:col-span-1 hidden rounded md:flex justify-center">
+              <div>
 
-                  <Image src={profileImg} alt='TEst' width="350px" height="350px" />
-                </div>
+                <Image src={profileImg} alt='TEst' width="350px" height="350px" />
               </div>
             </div>
           </div>
-
-          <Footer />
-
         </div>
+
+        <Footer />
+
       </div>
+    </div>
   )
 }
 
