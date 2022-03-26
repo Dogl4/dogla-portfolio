746be96094b465a8b59971a4aaa2757065dd7f43
Dogl4 <doougllas@hotmail.com.br>
2022-03-26 00:16:57 -0300

    Update `projects.js`:(pages). Update hover in projects

--- src/components/Projects/Card.js
+++ src/components/Projects/Card.js
@@ -1,23 +1,29 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import Image from "next/image";
-import styles from "../../styles/Home.module.css";
 
 const Card = ({ current }) => {
   const [isHover, setHover] = useState('hidden');
+  const [isHoverBlock, setHoverBlock] = useState('hidden');
 
   return (
     <div
-      onMouseOver={e => e.target && setHover('flex')}
-      onMouseOut={e => e.target && setHover('hidden')}
-      className="bg-gray-700 text-gray-200 relative rounded-2xl items-stretch col-span-1 row-span-3 hover:bg-sky-800 hover:text-white"
+      onMouseOver={e => e.target && (setHover('flex'), setHoverBlock('block'))}
+      onMouseOut={e => e.target && (setHover('hidden'), setHoverBlock('hidden'))}
+      className="text-gray-200 relative rounded-2xl items-stretch col-span-1 row-span-3 hover:bg-opacity-70"
     >
-      <div className="flex justify-around">
+      <div className={`${isHover} absolute p-1 z-30 bg-gray-500 w-[100%] rounded-t-2xl justify-around items-center`}>
         <div>{`${current.name}`}</div>
         <div>{`${current.type}`}</div>
       </div>
-      {current.description && <div className={`${isHover} absolute z-10 bg-slate-900 h-[84%] p-5 items-center`}>{`Description: ${current.description}`}</div>}
+      {(current.description || current.categories)
+        && <div className={`${isHover} absolute z-10 bg-gray-900 hover:bg-opacity-80 rounded-2xl h-[100%] items-center`}>
+          <div className={`${isHoverBlock} px-4`}>
+            {current.description && <p className='my-3'><strong>Descrição:</strong> {current.description}</p>}
+            {current.categories && <p><strong>Tecnologias:</strong>{current.categories.map((e) => (` ${e},`))}</p>}
+          </div>
+        </div>}
       {current.img && (<Image src={current.img} alt={current.name} className="rounded-2xl" width="420rem" height="260rem" />)}
-      <div className="flex justify-around content-end">
+      <div className={`${isHover} absolute p-1 z-30 bottom-0 bg-gray-500 w-[100%] rounded-b-2xl justify-around content-end`}>
         <div><a target="_blank" className='underline' href={current.urlRepo} rel="noreferrer">Link repositório</a></div>
         {current.urlSite && (<div><a target="_blank" className='underline' href={current.urlSite} rel="noreferrer">Link site</a></div>)}
       </div>
